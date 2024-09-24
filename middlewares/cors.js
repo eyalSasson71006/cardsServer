7b730378f573422713bbfd186fe56acbcb45ba4b
eyal71006 <eyal71006@gmail.com>
2024-09-24 14:54:40 +0300

    updated cors policy

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5173"],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5173", "https://cardsproject-ru9w.onrender.com"],
     })
 
 module.exports = corsMiddleware;
